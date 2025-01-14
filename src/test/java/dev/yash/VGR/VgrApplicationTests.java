f91a972e0d3149c337ec09948ad8e57631ec02d5
yash-sa1 <84633885+yash-sa1@users.noreply.github.com>
2025-01-14 20:10:39 +0000

    initial commit and setup

---
+++ src/test/java/dev/yash/VGR/VgrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yash.VGR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
